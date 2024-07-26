8935e64c642733035a5a6cf74585086e65ffd385
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-26 09:54:21 -0300

    feat: add swagger setup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import swaggerDocument from "../swagger.json";
 import { router } from "./routes";
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
-app.use(router);
+app.use(cors());
+app.use("v1", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(
   (
@@ -27,6 +34,12 @@ app.use(
     });
   }
 );
+
+app.get("/terms", (request: Request, response: Response) => {
+  return response.json({
+    message: "Termos de serviços",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}!`);
