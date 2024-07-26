00591501f3453b53247e0be843f27638eb477a2b
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-26 11:21:27 -0300

    fix: add / before v1

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const port = 3333;
 
 app.use(express.json());
 app.use(cors());
-app.use("v1", router);
+app.use("/v1", router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
